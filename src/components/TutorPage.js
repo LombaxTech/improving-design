998c8f386af06bcb55e79f52caf2c3fe51872f68
Rakib Khan <rakibkhan@live.co.uk>
2020-10-09 23:47:41 +0100

    .

--- src/components/TutorPage.js
+++ src/components/TutorPage.js
@@ -2,8 +2,8 @@ import React from "react";
 import DatePicker from "react-datepicker";
 
 export default function TutorPage(props) {
-    console.log(props);
-
+    // console.log(props);
+    
     const [startDate, setStartDate] = React.useState(new Date());
 
     return (
