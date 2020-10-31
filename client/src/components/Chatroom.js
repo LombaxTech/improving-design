ec1ec39af56db12a36dfda08f3c41524a3ed50ae
Rakib Khan <rakibkhan@live.co.uk>
2020-10-31 01:28:00 +0000

    .

--- client/src/components/Chatroom.js
+++ client/src/components/Chatroom.js
@@ -2,11 +2,74 @@ import React from "react";
 import "./stylesheets/chat.scss";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 export default function Chatroom() {
     return (
         <div className="chatroom">
             <h1>chatroom</h1>
+            <div className="messages">
+                <div className="message user-message">
+                    <Typography variant="body">Hi!</Typography>
+                </div>
+                <div className="message partner-message">
+                    <Typography variant="body">Hey, whats up?</Typography>
+                </div>{" "}
+                <div className="message user-message">
+                    <Typography variant="body">Hi!</Typography>
+                </div>
+                <div className="message partner-message">
+                    <Typography variant="body">Hey, whats up?</Typography>
+                </div>{" "}
+                <div className="message user-message">
+                    <Typography variant="body">Hi!</Typography>
+                </div>
+                <div className="message partner-message">
+                    <Typography variant="body">Hey, whats up?</Typography>
+                </div>{" "}
+                <div className="message user-message">
+                    <Typography variant="body">Hi!</Typography>
+                </div>
+                <div className="message partner-message">
+                    <Typography variant="body">Hey, whats up?</Typography>
+                </div>{" "}
+                <div className="message user-message">
+                    <Typography variant="body">Hi!</Typography>
+                </div>
+                <div className="message partner-message">
+                    <Typography variant="body">Hey, whats up?</Typography>
+                </div>{" "}
+                <div className="message user-message">
+                    <Typography variant="body">Hi!</Typography>
+                </div>
+                <div className="message partner-message">
+                    <Typography variant="body">Hey, whats up?</Typography>
+                </div>{" "}
+                <div className="message user-message">
+                    <Typography variant="body">Hi!</Typography>
+                </div>
+                <div className="message partner-message">
+                    <Typography variant="body">Hey, whats up?</Typography>
+                </div>{" "}
+                <div className="message user-message">
+                    <Typography variant="body">Hi!</Typography>
+                </div>
+                <div className="message partner-message">
+                    <Typography variant="body">Hey, whats up?</Typography>
+                </div>{" "}
+                <div className="message user-message">
+                    <Typography variant="body">Hi!</Typography>
+                </div>
+                <div className="message partner-message">
+                    <Typography variant="body">Hey, whats up?</Typography>
+                </div>{" "}
+                <div className="message user-message">
+                    <Typography variant="body">Hi!</Typography>
+                </div>
+                <div className="message partner-message">
+                    <Typography variant="body">Hey, whats up?</Typography>
+                </div>
+            </div>
             <div className="send-message">
                 <TextField
                     className="message-input"
