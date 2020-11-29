21a150713e480a7506035f80fd40785e051f9d6e
Rakib Khan <rakibkhan@live.co.uk>
2020-11-29 23:22:17 +0000

    .

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+// import green from "@material-ui/core/colors/green";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#673ab7",
+        },
+        secondary: {
+            main: "#ffc400",
+        },
+    },
+});
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
